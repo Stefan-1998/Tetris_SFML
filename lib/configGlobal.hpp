839d4a1be3734ac357ab7f03ab5a264c3d1cef72
9102 <s.maier@oth-aw.de>
2022-01-27 17:57:13 +0100

    Globales Config File hinzugefügt mit Parameter für Würfel

---
+++ lib/configGlobal.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#define windowHight 700
+#define windowWidth 700
+
+
+#define blockWidth 15
+#define blockHight 15
+
+#define offsetLeft 5
+#define offsetRight 5
+#define offsetUp 5
+#define offsetDown 5
+
+#define position leftWall 100
