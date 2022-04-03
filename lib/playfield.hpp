0e1353e981008cf4652e656787d1400c8b1d2420
9102 <s.maier@oth-aw.de>
2022-04-03 20:52:11 +0200

    Tetrisabfrage hinzgefügt

--- lib/playfield.hpp
+++ lib/playfield.hpp
@@ -11,14 +11,18 @@ class Playfield
         Playfield(int width, int hight);
         ~Playfield();
         void draw(sf::RenderWindow * window);
+        void sort();
+        void checkTetris();
         std::vector <std::shared_ptr<Square>>  blocks;
 
+        void print();
 
     private:
        int m_width; 
        int m_hight; 
        sf::RectangleShape * playfield;
        sf::RectangleShape * frame;
+       int m_points;
 
 
 };
